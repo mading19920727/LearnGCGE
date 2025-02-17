ac2125d2ebf1c051310959661f4f58a1c42a5e20
zhangzhiyu1994 <zhiyu.zhang@cqbdri.pku.edu.cn>
2025-02-17 23:05:26 +0800

    fix: 修正编译器设置

--- config/linux_mpicc.cmake
+++ config/linux_mpicc.cmake
@@ -2,12 +2,10 @@
 find_package(MPI REQUIRED)
 if (MPI_FOUND)
     include_directories(${MPI_INCLUDE_PATH})
-    set(CMAKE_C_COMPILER ${MPI_C_COMPILER})
-    set(CMAKE_CXX_COMPILER ${MPI_CXX_COMPILER})
+    # 不需要手动设置编译器，find_package(MPI) 会自动设置
+    # set(CMAKE_C_COMPILER ${MPI_C_COMPILER})
+    # set(CMAKE_CXX_COMPILER ${MPI_CXX_COMPILER})
 endif()
-# complier
-SET(CMAKE_C_COMPILER "mpicc")
-SET(CMAKE_CXX_COMPILER "mpicpc")
 
 # complie flag
 SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC  -std=c99")
