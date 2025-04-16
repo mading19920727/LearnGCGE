1d111893332ec727d028e7ff985ca03a7e3164af
mading <857327859@qq.com>
2025-04-16 19:11:10 +0800

    feat: 使用mumps求[a, b]区间内特征值个数

--- src/solver/count_eigen_num.cpp
+++ src/solver/count_eigen_num.cpp
@@ -7,63 +7,59 @@
 #include "count_eigen_num.h"
 
 GcgeErrCode CountEigenNum::countEigenNum(void *A, void *B, double a, double b, int &numEigen) {
-    printf("----countEigenNum\n");
-    PetscMPIInt rank, size; // 进程信息 
-    PetscInt rstart, rend;  // 当前进程所拥有的第一行/最后一行的全局索引
-    MatGetOwnershipRange((Mat)A, &rstart, &rend);
-
-    Mat A_aB;       // 保存 A-a*B
-    Mat A_aB_AIJ;   // 转数据格式
+    if (a > b) {    // [a, b]区间,应该 a =< b
+        return GCGE_ERR_INPUT;  
+    }
+    
+    /*计算cholesky(A - a * B),并获得惯性数值*/
+    // 计算A - a * B
+    Mat A_aB;   // 保存A - a * B
+    MatDuplicate((Mat)B, MAT_COPY_VALUES, &A_aB);           // 拷贝B，作B = A - a * B
+    MatAYPX(A_aB, -a, (Mat)A, DIFFERENT_NONZERO_PATTERN);   // MatAYPX(Y, a, X, ..)功能为 Y = a * Y + X 
+    
+    // 执行cholesky分解
+    IS row, col;                // 分解排序索引
+    MatFactorInfo info;         // 用于排序
+    PetscInt nneg, nzero, npos; // 保存惯性数值
     Mat chol_AaB;   // cholesky分解后的矩阵
 
-    MatDuplicate((Mat)B, MAT_COPY_VALUES, &A_aB);               // 拷贝B，作B = A - a*B
-    MatAYPX(A_aB, -a,  (Mat)A, DIFFERENT_NONZERO_PATTERN);      // MatAYPX(Y, a, X, ..)功能为 Y = a * Y + X 
-    MatConvert(A_aB, MATAIJ, MAT_INITIAL_MATRIX, &A_aB_AIJ);    // 只支持AIJ格式矩阵分解
-    
-    IS row, col;
-    MatFactorInfo info;
-    PetscInt nneg, nzero, npos;
+    // mumps参数设置，才能获得全局惯性
+    PetscOptionsInsertString(NULL, "-mat_mumps_icntl_24 1"); 
 
-    MatGetOrdering(A_aB_AIJ, MATORDERINGRCM, &row, &col);       // 矩阵排序
-    MatFactorInfoInitialize(&info);                             // MatCholeskyFactor(A_aB, row, &info); // 自带排序
-    MatGetFactor(A_aB_AIJ, MATSOLVERPETSC, MAT_FACTOR_CHOLESKY, &chol_AaB);
-    MatCholeskyFactorSymbolic(chol_AaB, A_aB_AIJ, row, &info);  // 符号分析
-    MatCholeskyFactorNumeric(chol_AaB, A_aB_AIJ, &info);        // 数值分解
-    
-    // 计算A - a * B 惯性指数
+    MatFactorInfoInitialize(&info);           
+    MatGetFactor(A_aB, MATSOLVERMUMPS, MAT_FACTOR_CHOLESKY, &chol_AaB); // mumps的cholesky分解
+              
+    MatGetOrdering(A_aB, MATORDERINGND, &row, &col);        // 排序
+    MatCholeskyFactorSymbolic(chol_AaB, A_aB, row, &info);  // 符号分析
+    MatCholeskyFactorNumeric(chol_AaB, A_aB, &info);        // 数值分解
+
+    // 计算惯性指数
     MatGetInertia(chol_AaB, &numEigen, &nzero, &npos); 
-    //printf("    nneg: %d, nzero:%d, npos: %d\n", numEigen, nzero, npos);
+    // --------------------------------------------------------------------------------------
 
-    /*-----------------------------------------------------------------------------------*/
-
-    Mat A_bB;       // 保存 A-b*B
-    Mat A_bB_AIJ;   // 转数据格式
-    Mat chol_AbB;   // cholesky分解后的矩阵
-
+    /*计算cholesky(A - b * B),并获得惯性数值*/
+    // 计算A - b * B
+    Mat A_bB;   // 保存A - b * B
     MatDuplicate((Mat)B, MAT_COPY_VALUES, &A_bB); 
-    MatAYPX(A_bB, -b,  (Mat)A, DIFFERENT_NONZERO_PATTERN);      
-    MatConvert(A_bB, MATAIJ, MAT_INITIAL_MATRIX, &A_bB_AIJ);
-    MatGetOrdering(A_bB_AIJ, MATORDERINGRCM, &row, &col);    
+    MatAYPX(A_bB, -b, (Mat)A, DIFFERENT_NONZERO_PATTERN);   // 因为MatAYPX(Y, a, X, ..)功能为 Y = a * Y + X，不能复用A_aB   
 
-    MatGetFactor(A_bB_AIJ, MATSOLVERPETSC, MAT_FACTOR_CHOLESKY, &chol_AbB);
-    MatCholeskyFactorSymbolic(chol_AbB, A_bB_AIJ, row, &info);
-    MatCholeskyFactorNumeric(chol_AbB, A_bB_AIJ, &info);
+    Mat chol_AbB;   // cholesky分解后的矩阵
+    MatGetFactor(A_bB, MATSOLVERMUMPS, MAT_FACTOR_CHOLESKY, &chol_AbB); 
+    MatCholeskyFactorSymbolic(chol_AbB, A_bB, row, &info);  // 复用了info
+    MatCholeskyFactorNumeric(chol_AbB, A_bB, &info);
 
-    // 计算A - b * B 惯性指数
+    // 计算惯性指数
     MatGetInertia(chol_AbB, &nneg, &nzero, &npos); 
-    //printf("    nneg: %d, nzero:%d, npos: %d\n", nneg, nzero, npos);
 
     // 区间内特征值个数
+    int tempN = numEigen;
     numEigen = nneg - numEigen;
-    //printf("[%lg, %lg]    numEigen: %d\n", numEigen);
-    PetscPrintf(PETSC_COMM_WORLD, "The number of eigenvalues in [a, b] = [%lg, %lg] is: %d \n", a, b, numEigen);
-    
+    PetscPrintf(PETSC_COMM_WORLD, "The number of eigenvalues in [a, b] is: %d = %d - %d\n", numEigen, nneg, tempN);
+
     ISDestroy(&row);
     ISDestroy(&col);
     MatDestroy(&A_aB);
     MatDestroy(&A_bB);
-    MatDestroy(&A_aB_AIJ);
-    MatDestroy(&A_bB_AIJ);
     MatDestroy(&chol_AaB);
     MatDestroy(&chol_AbB);
 
