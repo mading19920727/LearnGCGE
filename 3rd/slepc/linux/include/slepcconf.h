893e25defedf98bfcaf75106793a1cfab2cbd91b
ZhiyuZhang1994 <z425368988@gmail.com>
2025-04-17 17:02:03 +0800

    feat: slepc适配带mumps的petsc

--- 3rd/slepc/linux/include/slepcconf.h
+++ 3rd/slepc/linux/include/slepcconf.h
@@ -1,12 +1,11 @@
 #if !defined(INCLUDED_SLEPCCONF_H)
 #define INCLUDED_SLEPCCONF_H
 
-#define SLEPC_PETSC_DIR "/home/ma/myCode/petsc-3.22.3"
-#define SLEPC_PETSC_ARCH "petsc_arch"
-#define SLEPC_DIR "/home/ma/myCode/slepc-3.22.2"
-#define SLEPC_LIB_DIR "/home/ma/myCode/slepc-3.22.2/petsc_arch/lib"
-#define SLEPC_MISSING_LAPACK_GGSVD3 1
-#define SLEPC_MISSING_LAPACK_GGEV3 1
-#define SLEPC_MISSING_LAPACK_GGES3 1
-#define SLEPC_HAVE_PACKAGES ":"
+#define SLEPC_PETSC_DIR "/home/zzy/software/petsc-3.22.3"
+#define SLEPC_PETSC_ARCH "arch-linux-c-opt"
+#define SLEPC_DIR "/home/zzy/deps/slepc_mumps-3.22.2"
+#define SLEPC_LIB_DIR "/home/zzy/deps/slepc_mumps-3.22.2/lib"
+#define SLEPC_HAVE_SCALAPACK 1
+#define SLEPC_SCALAPACK_HAVE_UNDERSCORE 1
+#define SLEPC_HAVE_PACKAGES ":scalapack:"
 #endif
